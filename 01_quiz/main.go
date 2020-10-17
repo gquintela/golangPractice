b2eb2e35e99577a463ed620b96b07a5cf273bba6
gquintela <gquintela@dc.uba.ar>
2020-10-17 16:36:13 -0300

    parseLines method created

--- 01_quiz/main.go
+++ 01_quiz/main.go
@@ -1,13 +1,52 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"os"
 )
 
 func main() {
 
-	csvFilename := flag.String("filename", "algo por defecto", "enter a filename (must be .csv)")
+	filename := flag.String("filename", "problems.csv", "enter a filename (must be .csv)")
 	flag.Parse()
-	fmt.Println(*csvFilename)
+	fmt.Println(*filename)
+
+	//read file
+	file, err := os.Open(*filename)
+	if err != nil {
+		exit(fmt.Sprintf("Error: can't open %s", *filename))
+	}
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Error: check the input file content.")
+	}
+	problems := parseLines(lines)
+	fmt.Println(problems)
+
+
+}
+
+func exit(message string) {
+	fmt.Sprintln(message)
+	os.Exit(1)
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: line[1],
+		}
+	}
+	return ret
 }
