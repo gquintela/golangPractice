9dcd7b5fd08f36672066f26aece633308e14b2b7
gquintela <gquintela@dc.uba.ar>
2020-10-17 17:26:34 -0300

    Basic structure finished.

--- 01_quiz/main.go
+++ 01_quiz/main.go
@@ -3,20 +3,25 @@ package main
 import (
 	"encoding/csv"
 	"flag"
-	"fmt"
+	. "fmt"
 	"os"
+	"strings"
 )
+
+const red = "\033[31m"
+const green = "\033[32m"
+const black = "\033[0m"
 
 func main() {
 
 	filename := flag.String("filename", "problems.csv", "enter a filename (must be .csv)")
 	flag.Parse()
-	fmt.Println(*filename)
+	Println(*filename)
 
 	//read file
 	file, err := os.Open(*filename)
 	if err != nil {
-		exit(fmt.Sprintf("Error: can't open %s", *filename))
+		exit(Sprintf("Error: can't open %s", *filename))
 	}
 	r := csv.NewReader(file)
 	lines, err := r.ReadAll()
@@ -24,13 +29,27 @@ func main() {
 		exit("Error: check the input file content.")
 	}
 	problems := parseLines(lines)
-	fmt.Println(problems)
 
+	var score int
+	var userAnswer string
+	for i, problem := range problems {
+		Printf("Problem number %d: %s? ", i+1, problem.q)
+		_, _ = Scanf("%s ", &userAnswer)
+		userAnswer = strings.TrimSpace(userAnswer)
+		if userAnswer == problem.a {
+			score++
+			Println(string(green), "OK!")
+		} else {
+			Println(string(red), "Wrong!")
+		}
+		Print(string(black))
+	}
+	Printf("\nYour score: %d out of %d\n", score, len(problems))
 
 }
 
 func exit(message string) {
-	fmt.Sprintln(message)
+	Sprintln(message)
 	os.Exit(1)
 }
 
@@ -44,8 +63,8 @@ func parseLines(lines [][]string) []problem {
 
 	for i, line := range lines {
 		ret[i] = problem{
-			q: line[0],
-			a: line[1],
+			q: strings.TrimSpace(line[0]),
+			a: strings.TrimSpace(line[1]),
 		}
 	}
 	return ret
